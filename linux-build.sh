21cc4336cd9c29cd63c0f35548780463d133bdda
Katie Barrett-Powell <Barrett-PowellK@cardiff.ac.uk>
2020-04-07 12:54:36 +0100

    Update linux-build.sh

--- linux-build.sh
+++ linux-build.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 cd MapServer &&
 sudo docker run -p 8081:80 -v openstreetmap-data:/var/lib/postgresql/12/main -v openstreetmap-rendered-tiles:/var/lib/mod_tile -e ALLOW_CORS=1 -d overv/openstreetmap-tile-server run &&
+cd ../chatroom &&
+sudo apt install cmdtest &&
 cd ../ &&
 npm run dev-build &&
 cd Rasa/venv &&
