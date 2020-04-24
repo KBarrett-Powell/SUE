c773000a1aecb1ebc0058b9e2a0a344f99e721ae
Katie Barrett-Powell <Barrett-PowellK@cardiff.ac.uk>
2020-04-24 21:19:16 +0100

    added more cross-origin allowances

--- linux-run.sh
+++ linux-run.sh
@@ -2,5 +2,5 @@
 fuser -k 8000/tcp
 fuser -k 8080/tcp
 gnome-terminal -- bash -c "cd Rasa && source ./venv/bin/activate && rasa run actions" &&
-gnome-terminal -- bash -c "cd Rasa && source ./venv/bin/activate && python -m rasa run --m ./models --endpoints endpoints.yml --port 5052 -vv --enable-api " &&
+gnome-terminal -- bash -c "cd Rasa && source ./venv/bin/activate && python -m rasa run --m ./models --endpoints endpoints.yml --port 5052 -vv --enable-api --cors '*'" &&
 npm run dev-start
