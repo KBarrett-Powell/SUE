e45dc5fa6034b6b1fb243d83ffb492f2d7781f54
KBarrett-Powell <Barrett-PowellK@cardiff.ac.uk>
2020-06-24 17:50:21 +0100

    Updated Map Markers

--- WebInterface/js/getmarkers.js
+++ WebInterface/js/getmarkers.js
@@ -2,31 +2,6 @@ const sensorsurl = "http://localhost:8000/sensors";
 const eventsurl = "http://localhost:8000/events";
 const complexurl = "http://localhost:8000/complex";
 
-const sensoralist = document.getElementById("sensoralist");
-const sensorclist = document.getElementById("sensorclist");
-const sensormlist = document.getElementById("sensormlist");
-const sensorplist = document.getElementById("sensorplist");
-const eventalist = document.getElementById("eventalist");
-const eventplist = document.getElementById("eventplist");
-const eventvlist = document.getElementById("eventvlist");
-const eventllist = document.getElementById("eventllist");
-const complexalist = document.getElementById("complexalist");
-
-const slstbadge = document.getElementById("slst-badge");
-const salstbadge = document.getElementById("salst-badge");
-const sclstbadge = document.getElementById("sclst-badge");
-const smlstbadge = document.getElementById("smlst-badge");
-const splstbadge = document.getElementById("splst-badge");
-
-const elstbadge = document.getElementById("elst-badge");
-const ealstbadge = document.getElementById("ealst-badge");
-const eplstbadge = document.getElementById("eplst-badge");
-const evlstbadge = document.getElementById("evlst-badge");
-const ellstbadge = document.getElementById("ellst-badge");
-
-const clstbadge = document.getElementById("clst-badge");
-const calstbadge = document.getElementById("calst-badge");
-
 window.sensorCamera = L.layerGroup();
 window.sensorMicrophone = L.layerGroup();
 window.sensorHuman = L.layerGroup();
@@ -50,101 +25,90 @@ window.prvClickedMarker = null;
 
 // Define camera icon
 const cameraIcon = L.icon({
-    iconUrl: 'images/camera pin.svg',
+    iconUrl: 'images/camera-marker.png',
     shadowUrl: "images/marker-shadow.png",
 
-    iconSize:     [28, 49],
-    shadowSize:   [49, 49],
-    iconAnchor:   [14, 48],
-    popupAnchor:  [0, -42] 
+    iconSize:     [35, 54],
+    shadowSize:   [54, 54],
+    iconAnchor:   [17, 53],
+    popupAnchor:  [0, -48] 
 });
 
 // Define microphone icon
 const microphoneIcon = L.icon({
-    iconUrl: 'images/microphone pin.svg',
+    iconUrl: 'images/microphone-marker.png',
     shadowUrl: "images/marker-shadow.png",
 
-    iconSize:     [28, 49],
-    shadowSize:   [49, 49],
-    iconAnchor:   [14, 48],
-    popupAnchor:  [0, -42] 
+    iconSize:     [35, 54],
+    shadowSize:   [54, 54],
+    iconAnchor:   [17, 53],
+    popupAnchor:  [0, -48] 
 });
 
 // Define human icon
 const humanIcon = L.icon({
-    iconUrl: 'images/human pin.svg',
+    iconUrl: 'images/human-marker.png',
     shadowUrl: "images/marker-shadow.png",
 
-    iconSize:     [28, 49],
-    shadowSize:   [49, 49],
-    iconAnchor:   [14, 48],
-    popupAnchor:  [0, -42] 
+    iconSize:     [35, 54],
+    shadowSize:   [54, 54],
+    iconAnchor:   [17, 53],
+    popupAnchor:  [0, -48] 
 });
 
 // Define blue marker
 const blueIcon = L.icon({
-    iconUrl: 'images/blue pin.svg',
+    iconUrl: 'images/blue-marker.png',
     shadowUrl: "images/marker-shadow.png",
 
-    iconSize:     [28, 49],
-    shadowSize:   [49, 49],
-    iconAnchor:   [14, 48],
-    popupAnchor:  [0, -42] 
-});
-
-// Define green marker
-const greenIcon = L.icon({
-    iconUrl: 'images/marker-green.png',
-    shadowUrl: "images/marker-shadow.png",
-
-    iconSize:     [28, 49],
-    shadowSize:   [49, 49],
-    iconAnchor:   [14, 48],
-    popupAnchor:  [0, -42] 
+    iconSize:     [35, 54],
+    shadowSize:   [54, 54],
+    iconAnchor:   [17, 53],
+    popupAnchor:  [0, -48] 
 });
 
 // Define yellow marker
 const yellowIcon = L.icon({
-    iconUrl: 'images/yellow pin.svg',
+    iconUrl: 'images/yellow-marker.png',
     shadowUrl: "images/marker-shadow.png",
 
-    iconSize:     [28, 49],
-    shadowSize:   [49, 49],
-    iconAnchor:   [14, 48],
-    popupAnchor:  [0, -42] 
+    iconSize:     [35, 54],
+    shadowSize:   [54, 54],
+    iconAnchor:   [17, 53],
+    popupAnchor:  [0, -48]  
 });
 
 // Define orange marker
 const orangeIcon = L.icon({
-    iconUrl: 'images/orange pin.svg',
+    iconUrl: 'images/orange-marker.png',
     shadowUrl: "images/marker-shadow.png",
 
-    iconSize:     [28, 49],
-    shadowSize:   [49, 49],
-    iconAnchor:   [14, 48],
-    popupAnchor:  [0, -42] 
+    iconSize:     [35, 54],
+    shadowSize:   [54, 54],
+    iconAnchor:   [17, 53],
+    popupAnchor:  [0, -48]  
 });
 
 // Define red marker
 const redIcon = L.icon({
-    iconUrl: 'images/red pin.svg',
+    iconUrl: 'images/red-marker.png',
     shadowUrl: "images/marker-shadow.png",
 
-    iconSize:     [28, 49],
-    shadowSize:   [49, 49],
-    iconAnchor:   [14, 48],
-    popupAnchor:  [0, -42] 
+    iconSize:     [35, 54],
+    shadowSize:   [54, 54],
+    iconAnchor:   [17, 53],
+    popupAnchor:  [0, -48] 
 });
 
 // Define complex marker
 const complexIcon = L.icon({
-    iconUrl: 'images/complex pin.svg',
+    iconUrl: 'images/complex-marker.png',
     shadowUrl: "images/marker-shadow.png",
 
-    iconSize:     [28, 49],
-    shadowSize:   [49, 49],
-    iconAnchor:   [14, 48],
-    popupAnchor:  [0, -42] 
+    iconSize:     [35, 54],
+    shadowSize:   [54, 54],
+    iconAnchor:   [17, 53],
+    popupAnchor:  [0, -48]  
 });
 
 // Define selected camera icon
@@ -201,118 +165,57 @@ function addMarker(json, iconimg) {
         }
     
     } else {
-        let radius = window.complexDist;
+        let mainradius = window.complexDist;
+        //let radius = json.properties.uncertainty;
 
         let iconChoice = null;
         let colourChoice = "";
 
         if (json.properties.priority == 4) {
             iconChoice = blueIcon;
-            colourChoice = "#3388ff";
+            colourChoice = "#76caec";
 
         } else if (json.properties.priority == 3) {
             iconChoice = yellowIcon;
-            colourChoice = "#ffff4d";
+            colourChoice = "#fedd80";
         
         } else if (json.properties.priority == 2) {
             iconChoice = orangeIcon;
-            colourChoice = "#ff6600";
+            colourChoice = "#fea080";
         
         } else if (json.properties.priority == 1) {
             iconChoice = redIcon;
-            colourChoice = "#ff0000";
+            colourChoice = "#fe7f7f";
         }
 
         let eventmarker = L.marker(coordinates, {icon: iconChoice, properties: JSON.stringify(json.properties)}).on('click', toggleDetailsFromMap);
-        let range1 = L.circle(coordinates, {radius: radius, fillColor: colourChoice, color: colourChoice, fillOpacity: 0.3, weight: 0.3, gradient: true});
-        // let range1 = L.circle(coordinates, {radius: radius/3, fillColor: colourChoice, color: colourChoice, fillOpacity: 0.1, weight: 0.3, gradient: true});
-        // let range2 = L.circle(coordinates, {radius: (radius*2)/3, fillColor: colourChoice, color: colourChoice, fillOpacity: 0.1, weight: 0.5, gradient: true});
-        // let range3 = L.circle(coordinates, {radius: radius, fillColor: colourChoice, color: colourChoice, fillOpacity: 0.1, weight: 0.7, gradient: true});
+        //let range1 = L.circle(coordinates, {radius: mainradius, fillColor: colourChoice, color: colourChoice, fillOpacity: 0.3, weight: 0.3, gradient: true});
+        // let range1 = L.circle(coordinates, {radius: mainradius/3, fillColor: colourChoice, color: colourChoice, fillOpacity: 0.3, weight: 0.2, gradient: true});
+        // let range2 = L.circle(coordinates, {radius: mainradius*2/3, fillColor: colourChoice, color: colourChoice, fillOpacity: 0.2, weight: 0.4, gradient: true});
+        // let range3 = L.circle(coordinates, {radius: mainradius, fillColor: colourChoice, color: colourChoice, fillOpacity: 0.2, weight: 0.6, gradient: true});
+        let range1 = L.circle(coordinates, {radius: mainradius*2/5, fillColor: colourChoice, color: colourChoice, fillOpacity: 0.6, weight: 3, gradient: true});
+        let range2 = L.circle(coordinates, {radius: mainradius, fillColor: colourChoice, color: colourChoice, fillOpacity: 0.4, weight: 3, gradient: true});
 
         eventmarker.bindPopup(json.properties.eventName)
         
         if (json.properties.eventType == "Person") {
             eventmarker.addTo(window.eventPerson);
             range1.addTo(window.personEventRange);
-            //range2.addTo(window.personEventRange);
+            range2.addTo(window.personEventRange);
             //range3.addTo(window.personEventRange);
 
         } else if (json.properties.eventType == "Vehicle") {
             eventmarker.addTo(window.eventVehicle);
             range1.addTo(window.vehicleEventRange);
-            //range2.addTo(window.vehicleEventRange);
+            range2.addTo(window.vehicleEventRange);
             //range3.addTo(window.vehicleEventRange);
 
         } else {
             eventmarker.addTo(window.eventPlanned);
             range1.addTo(window.plannedEventRange);
-            //range2.addTo(window.plannedEventRange);
+            range2.addTo(window.plannedEventRange);
             //range3.addTo(window.plannedEventRange);
         }
-    }
-}
-
-// Add marker function
-function addListItem(json, type, specificType) {
-
-    let name = (type == "Event") ? json.eventName : ((type == "Sensor") ? json.sensorName : json.complexName);
-    
-    let span = document.createElement('span');
-    let button = document.createElement('button');
-    button.onclick = function(){toggleDetailsFromPage(this)};
-    button.id = JSON.stringify(json);
-    let listitem = document.createElement('div');
-    listitem.classList = "list-group-item listitem";
-    span.appendChild(document.createTextNode(name));
-    button.appendChild(span);
-    listitem.appendChild(button);
-
-    let span2 = document.createElement('span');
-    let button2 = document.createElement('button');
-    button2.onclick = function(){toggleDetailsFromPage(this)};
-    button2.id = JSON.stringify(json);
-    let listitem2 = document.createElement('div');
-    listitem2.classList = "list-group-item listitem";
-    span2.appendChild(document.createTextNode(name));
-    button2.appendChild(span2);
-    listitem2.appendChild(button2);
-
-    if (type === "Sensor") {
-        if (specificType === "Camera") {
-            sensorclist.appendChild(listitem);
-            sclstbadge.innerHTML = (parseInt(sclstbadge.innerHTML) + 1).toString();
-        } else if (specificType === "Microphone") {
-            sensormlist.appendChild(listitem);
-            smlstbadge.innerHTML = (parseInt(smlstbadge.innerHTML) + 1).toString();
-        } else {
-            sensorplist.appendChild(listitem);
-            splstbadge.innerHTML = (parseInt(splstbadge.innerHTML) + 1).toString();
-        }
-
-        sensoralist.appendChild(listitem2);
-        salstbadge.innerHTML = (parseInt(salstbadge.innerHTML) + 1).toString();
-        slstbadge.innerHTML = (parseInt(slstbadge.innerHTML) + 1).toString();
-
-    } else if (type === "Event") {
-        if (specificType === "Person") {
-            eventplist.appendChild(listitem);
-            eplstbadge.innerHTML = (parseInt(eplstbadge.innerHTML) + 1).toString();
-        } else if (specificType === "Vehicle") {
-            eventvlist.appendChild(listitem);
-            evlstbadge.innerHTML = (parseInt(evlstbadge.innerHTML) + 1).toString();
-        } else {
-            eventllist.appendChild(listitem);
-            ellstbadge.innerHTML = (parseInt(ellstbadge.innerHTML) + 1).toString();
-        }
-
-        eventalist.appendChild(listitem2);
-        ealstbadge.innerHTML = (parseInt(ealstbadge.innerHTML) + 1).toString();
-        elstbadge.innerHTML = (parseInt(elstbadge.innerHTML) + 1).toString();
-
-    } else {
-        complexalist.appendChild(listitem);
-        calstbadge.innerHTML = (parseInt(calstbadge.innerHTML) + 1).toString();
-        clstbadge.innerHTML = (parseInt(clstbadge.innerHTML) + 1).toString();
     }
 }
 
