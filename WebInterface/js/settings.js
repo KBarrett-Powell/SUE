06f5963ac1b2f4c87b3d00ff80aa4f9a234e9b4c
KBarrett-Powell <Barrett-PowellK@cardiff.ac.uk>
2020-07-09 14:12:59 +0100

    Removed Unused Code

--- WebInterface/js/settings.js
+++ WebInterface/js/settings.js
@@ -59,20 +59,3 @@ function toggleAccessibility(on) {
 
     }
 };
-
-function simpleStringify (object){
-    var simpleObject = {};
-    for (var prop in object ){
-        if (!object.hasOwnProperty(prop)){
-            continue;
-        }
-        if (typeof(object[prop]) == 'object'){
-            continue;
-        }
-        if (typeof(object[prop]) == 'function'){
-            continue;
-        }
-        simpleObject[prop] = object[prop];
-    }
-    return JSON.stringify(simpleObject); // returns cleaned up JSON
-};
