f255ae113a9242f6f3d72a22b41d455792b65485
KBarrett-Powell <Barrett-PowellK@cardiff.ac.uk>
2020-08-07 15:40:02 +0100

    Removed Uncessesary Code

--- WebInterface/js/ws-connect.js
+++ WebInterface/js/ws-connect.js
@@ -5,29 +5,6 @@ ws.onopen = function() {
   obj.type = "add-sue-client";
   ws.send(JSON.stringify(obj));
 
-  window.currentLayers = {
-    "ownerSensors": true,
-    "critPriorityEventRange": true,
-    "highPriorityEventRange": true,
-    "medPriorityEventRange": true,
-    "lowPriorityEventRange": true,
-    "sensorCameraRange": false,
-    "sensorMicrophoneRange": false,
-    "sensorHumanRange": false,
-    "sensorUKRange": true,
-    "sensorUSRange": true,
-    "sensorCamera": false,
-    "sensorMicrophone": false,
-    "sensorHuman": false,
-    "sensorUK": true,
-    "sensorUS": true,
-    "critPriorityEvent": true,
-    "highPriorityEvent": true,
-    "medPriorityEvent": true,
-    "lowPriorityEvent": true,
-    "complexEvent": true
-  }
-
   clearMap();
   initializeLayers();
   initiateTimeRefresh();
